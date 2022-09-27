6d056ce94db988abc6d74137d25c6ed40ab7ebe1
Daniel Garwood <daniel.garwood@bbc.co.uk>
2022-09-27 15:33:51 +0100

    added search for emails

--- extract.js
+++ extract.js
@@ -5,8 +5,13 @@ let text = fs.readFileSync("sample.txt", "utf-8")
 
 console.log("read", text.length, "characters.");
 
+// <local>@<domain>
+
 // example of information gathered
 let lineCount = 0;
+let emailCount = 0;
+
+let emails = ["@softwire.com", "@corndel.com", "@hotmail.co.uk", "@kiwimail.co.nz", "@gmail.com"];
 
 text.split("\n").forEach(
     processOneLine
@@ -14,12 +19,23 @@ text.split("\n").forEach(
 
 console.log(`Processed ${lineCount} lines.`);
 
+console.log(`Processed ${emailCount} emails.`);
+
 // extract information from one line
 // and update global variables
 function processOneLine(line) {
     // example of gathering information
+    
     lineCount++;
 
+    //console.log(line)
     // add code here to find emails in the line 
+    for (let i = 0; i <= line.length; i++) {
+        if(line.substring(i, line.length) == "@softwire.com"){
+            emailCount++;
+        }
+        if(line.substring(i, line.length) == "@corndel.com"){
+            emailCount++;
+        }
+    }
 }
-
