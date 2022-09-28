ccdfa65646e1b16279778948f968ed7345910356
Daniel Garwood <daniel.garwood@bbc.co.uk>
2022-09-28 11:29:55 +0100

    added regex search for emails

--- extract.js
+++ extract.js
@@ -1,41 +1,46 @@
 
-
 import fs from 'fs';
 let text = fs.readFileSync("sample.txt", "utf-8")
 
 console.log("read", text.length, "characters.");
 
-// <local>@<domain>
-
 // example of information gathered
 let lineCount = 0;
 let emailCount = 0;
 
-let emails = ["@softwire.com", "@corndel.com", "@hotmail.co.uk", "@kiwimail.co.nz", "@gmail.com"];
+// <local>@<domain>
+const email_re = /[A-Za-z0-9._%+-]+@([a-z0-9-]+[.])+[a-z]+/;
+// first part of email address - [A-Z0-9._%+-] can be letters/numbers/symbols
+// followed by "@" symbol - @([a-z0-9-]+[.]) followed by letters/numbers/certain symbols, followed by "."  ...
+// last part of the email - [a-z]+ final part of address
+
+const softwire = /@(<softwire.com>\w+)/mg;
+//const softwire = /@([<softwire.com>]).*?\1/g;
+
 
 text.split("\n").forEach(
     processOneLine
 )
 
 console.log(`Processed ${lineCount} lines.`);
-
 console.log(`Processed ${emailCount} emails.`);
 
 // extract information from one line
 // and update global variables
-function processOneLine(line) {
+function processOneLine(line, email_search) {
     // example of gathering information
-    
-    lineCount++;
 
-    //console.log(line)
+    lineCount++;
+    email_search = line.search(email_re);
+
+
     // add code here to find emails in the line 
-    for (let i = 0; i <= line.length; i++) {
-        if(line.substring(i, line.length) == "@softwire.com"){
-            emailCount++;
-        }
-        if(line.substring(i, line.length) == "@corndel.com"){
-            emailCount++;
-        }
+    if (email_search) {
+        emailCount++;
+        console.log(email_search);
+    }
+
+    for (const match of line.matchAll(softwire)) {
+        console.log(`Processed ${match.groups.softwire.com}`);
     }
 }
